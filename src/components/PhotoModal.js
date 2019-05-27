cbb8cca551274ee641c6a024e7f68f2018881c9d
Vu Duy <duynvu@gmail.com>
2019-05-27 23:20:06 +0700

    feature: edit

--- src/components/PhotoModal.js
+++ src/components/PhotoModal.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PhotoModal.css';
 
 export default function PhotoModal(props) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleOnChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        if(name === "name") {
+            setName(value);
+        } else {
+            setDescription(name);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className="photoModal">
             <div>
@@ -11,10 +30,32 @@ export default function PhotoModal(props) {
                     <button style={{ float: 'right' }} onClick={props.nextModalImage}>Next</button>
                 </div>
             </div>
-            <div>
-                <p><strong>{props.modalImg.name}</strong></p>
-                <p>{props.modalImg.description}</p>
-            </div>
+            {!isEdit && <div className="photoModalInfo">
+                <div className="photoInfo">
+                    <p><strong>{props.modalImg.name}</strong></p>
+                    <p>{props.modalImg.description}</p>
+                </div>
+                <div className="photoEditButton">
+                    <button onClick={() => setIsEdit(true)}>Edit</button>
+                </div>
+            </div>}
+            {isEdit && <div class="photoModalForm">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" 
+                        value={props.modalImg.name} 
+                        onChange={handleOnChange}/>
+                    </div>
+                    <div>
+                        <label>Description</label>
+                        <input type="text" name="description" 
+                        value={props.modalImg.description} 
+                        onChange={handleOnChange}/>
+                    </div>
+                    <button onClick={() => setIsEdit(false)}>Submit</button>
+                </form>
+            </div>}
         </div>
     )
 }
